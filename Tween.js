34a8f38561eaa5ca57133b93b0f9eae0d5ebf5aa
browerjosiahdev <brower.josiah.dev@gmail.com>
2015-12-14 12:03:23 -0700

    Added an isPlaying property to watch for if the animation is currently playing

--- Tween.js
+++ Tween.js
@@ -33,30 +33,32 @@ if (console === undefined) {
 var Tween = function( element, property, easing, from, to, duration ) {
 	// Keep a handle to the given element.
 	this.p_element = element;
-	
+
+	// Get/Set properties.
+	this.isPlaying = false;
+
 	// Convert the given element to a jquery element.
 	element = $(element);
-	
+
 	var cssData = Object.create(null);
-	var _this   = this;
-		
+
 	// If the "from" value is defined, make sure to set the from value to the given property before setting
-	// up the transition styles. 
+	// up the transition styles.
 	if (from !== undefined && property !== undefined) {
 		cssData[property] = from;
-		
+
 		element.css(cssData);
 	}
-	
+
 	// Clear the css data object.
 	cssData = Object.create(null);
-	
+
 	// Set the "to" value for the given property in order to play the transition.
 	cssData[property] = to;
-	
+
 	if (this.checkSupport()) {
 		// Setup the css data object with the duration, property, and easing values, then set these as the css
-		// data for the given element. 
+		// data for the given element.
 		if (duration !== undefined && !isNaN(duration)) {
 			cssData.transitionDuration       = (duration / 1000) + 's';
 			cssData.oTransitionDuration      = (duration / 1000) + 's';
@@ -66,7 +68,7 @@ var Tween = function( element, property, easing, from, to, duration ) {
 		} else {
 			console.warn('Tween: cannot tween element without a defined duration.');
 		}
-		
+
 		if (property !== undefined) {
 			cssData.transitionProperty       = property;
 			cssData.oTransitionProperty      = property;
@@ -76,31 +78,37 @@ var Tween = function( element, property, easing, from, to, duration ) {
 		} else {
 			console.warn('Tween: cannot tween element without a defined property.');
 		}
-		
+
 		if (easing === undefined) {
 			easing = 'linear';
 		}
-		
+
 		cssData.transitionTimingFunction       = easing;
 		cssData.oTransitionTimingFunction      = easing;
 		cssData.msTransitionTimingFunction     = easing;
 		cssData.mozTransitionTimingFunction    = easing;
 		cssData.webkitTransitionTimingFunction = easing;
-		
+
 		element.css(cssData);
-		
+
 		cssData = Object.create(null);
-		
+
+		// Flag the tween as playing.
+		this.isPlaying = true;
+
 		// Watch for the css3 transition to complete, and set the css data for the element to begin the transition.
 		element.on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
-			_this.transitionComplete();
-		});
+			this.transitionComplete();
+		}.bind(this));
 		element.css(cssData);
 	} else if ($ !== undefined) {
+		// Flag the tween as playing.
+		this.isPlaying = true;
+
 		// Use the jQuery animate method to tween the element.
 		element.animate(cssData, duration, easing, function() {
-			_this.transitionComplete();
-		});
+			this.transitionComplete();
+		}.bind(this));
 	} else {
 		console.warn('Tween: CSS3 transitions not supported, and jQuery is not included.');
 	}
@@ -110,6 +118,9 @@ Tween.TWEEN_COMPLETE = 'TWEENCOMPLETE';
 Tween.DISPATCH_ERROR = 'Not able to dispatch event, please make sure you are including jQuery in your project: %function%';
 
 Tween.prototype.transitionComplete = function() {
+	// Flag the tween as not playing.
+	this.isPlaying = false;
+
 	// Try to dispatch the jquery event, or warn the user if it doesn't work.
 	try {
 		$(this).trigger({
@@ -129,17 +140,17 @@ Tween.prototype.checkSupport = function() {
         style    = element.style,
         property = 'transition';
 
-    if (typeof style[property] === 'string') { 
-		return true; 
+    if (typeof style[property] === 'string') {
+		return true;
 	}
 
     var vendors = ['Moz', 'webkit', 'Webkit', 'Khtml', 'O', 'ms'];
-	
+
     property = property.charAt(0).toUpperCase() + property.substr(1);
 
     for (var inVendor = 0; inVendor < vendors.length.length; inVendor++) {
-        if (typeof style[vendors[inVendor] + property] === 'string') { 
-			return true; 
+        if (typeof style[vendors[inVendor] + property] === 'string') {
+			return true;
 		}
     }
 
